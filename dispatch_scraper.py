b586d37eee6582497258792c276fc849d78290f9
wlanghorne <whlanghorne@gmail.com>
2022-04-12 16:45:37 -0500

    Adding kaggle upload/version control

--- dispatch_scraper.py
+++ dispatch_scraper.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-from scraper_functions import create_dir, create_file
+from scraper_functions import format_out_files, gather_latest_dispatches, intialize_temp_file, get_latest_dispatch, create_dir, create_file, create_metadata_file, write_to_kaggle
 from time import sleep
 from datetime import date
 import csv
@@ -13,6 +15,8 @@ import os
 output_path = './outputs'
 driver_path = './chromedriver'
 url = 'https://clrweb.littlerock.state.ar.us/pub/public_menu.php'
+kaggle_path = './kaggle'
+orgin_metadata_path = './dataset-metadata.json'
 
 # CSV headers 
 headers = ['Call type', 'Location', 'Dispatch time']
@@ -20,91 +24,68 @@ headers = ['Call type', 'Location', 'Dispatch time']
 # Create output directory if it doesn't already exist 
 create_dir(output_path)
 
+# Create the output directory for the current day if it doesn't already exist
+today = str(date.today())
+output_path = os.path.join(output_path, today)
+create_dir(output_path)
+
+# Create the final output directory for the current day if it doesn't already exist
+output_path = os.path.join(output_path, 'final')
+create_dir(output_path)
+
 # CSV final output file and temp storage file containing reports for the current day 
-out_file = str(date.today()) + '.csv'
-temp_file = str(date.today()) + '_temp.csv'
+out_file = today + '.csv'
+temp_file = today + '_temp.csv'
 out_file_path = os.path.join(output_path, out_file)
-temp_file_path = os.path.join(output_path, temp_file)
+temp_file_path = os.path.join(os.path.split(output_path)[0], temp_file)
 
-
-# If the csv file doesn't exist, create it and add headers
-create_file(out_file_path, headers)
+# If the csv file doesn't exist, create it w/ headers and generate meta data file for kaggle
+# Set boolean value for if new dataset depending on if new file was created 
+is_new_dataset = create_file(out_file_path, headers)
+create_metadata_file(kaggle_path, orgin_metadata_path, output_path, today)
 
 # Initiate driver
+chrome_options = Options()
+chrome_options.add_argument("--headless")
 s = Service(driver_path)
-driver = webdriver.Chrome(service=s)
+driver = webdriver.Chrome(service=s, options=chrome_options)
 driver.get(url)
 
+# Update status in terminal
+print("Opening url ...")
+
 # Delay to allow data to load 
-sleep(10)
+WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.CSS_SELECTOR, "a[class='paginate_button current']")))
 
 # Select all dispatches
 select_entries = Select(driver.find_element(By.NAME,'cadEventTable_length'))
 select_entries.select_by_value('-1')
 
+# Delay to allow data to load 
+WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.CSS_SELECTOR, "a[class='paginate_button previous disabled']")))
+
+# Update status in terminal 
+print("Getting latest saved dispatch ...")
 
 # Get latest dispatch time 
-latest_dispatch = ''
-with open(out_file_path, 'r') as f:
-    latest_dispatch_holder = False
-    reader = csv.reader(f)
-    # skip header 
-    next(reader)
-    # try to open second row and extract the last dispatch time
-    try: 
-        second_row = next(reader)
-        latest_dispatch = second_row[2]
-    except StopIteration as exception:
-        print(exception)
-        latest_dispatch = False
+latest_dispatch = get_latest_dispatch(out_file_path)
 
-with open(temp_file_path, 'w') as f:
-    writer = csv.writer(f)
-    writer.writerow(headers)
-    f.close()
+# Create or overwrite temp file 
+intialize_temp_file(temp_file_path, headers)
 
 # Get table rows 
 table_body = driver.find_element(By.CSS_SELECTOR,'tbody')
 rows = table_body.find_elements(By.CSS_SELECTOR,'tr')
 
-# Gather new dispatch calls 
-for row in rows:
-    # Get cell data 
-    cells = row.find_elements(By.CSS_SELECTOR, 'td')
-    cell_data = []
-    for cell in cells:
-        cell_data.append(cell.get_attribute('innerHTML'))
-        # Check if the latest data in the dispatch log matches the latest data in the sheet 
-    if cell_data[2] == latest_dispatch:
-        break 
-    else: 
-        # Write to file  
-        with open(temp_file_path, 'a') as f:
-            writer = csv.writer(f)
-            writer.writerow(cell_data)
-            f.close()
+# Update status in terminal 
+print("Gathering latest dispatches ...")
 
-# Write old dispatch calls beneath new calls in temp file 
-if latest_dispatch: 
-    with open(temp_file_path, 'a') as temp_f:
-        writer = csv.writer(temp_f)
-        with open(file_path, 'r') as f:
-            reader = csv.reader(f)
-            next(reader)
-            for row in reader:
-                writer.writerow(row)
-            f.close()
-        temp_f.close()
+gather_latest_dispatches(temp_file_path, rows, latest_dispatch)
 
-# Write temp file into file  
-with open(out_file_path, 'w') as f:
-    writer = csv.writer(f)
-    with open(temp_file_path, 'r') as temp_f:
-        reader = csv.reader(temp_f)
-        for row in reader:
-            writer.writerow(row)
-        temp_f.close()
-    f.close()
+# Update status in terminal 
+print("Formating output files ...")
 
+format_out_files(latest_dispatch, temp_file_path, out_file_path)
 
-
+# Write final data to kaggle 
+write_to_kaggle(is_new_dataset, kaggle_path, output_path)
--- scraper_functions.py
+++ scraper_functions.py
@@ -1,5 +1,9 @@
+from datetime import datetime
 import csv
+import json
 import os
+import shutil 
+from selenium.webdriver.common.by import By
 
 def create_dir(path):
 	if not os.path.isdir(path):
@@ -11,6 +15,8 @@ def create_file(path, headers):
 	        writer = csv.writer(f)
 	        writer.writerow(headers)
 	        f.close()
+	        # indicate that new file was created 
+	        return True 
 	# ensure headers are in file if file exists
 	else: 
 		try:
@@ -30,4 +36,101 @@ def create_file(path, headers):
 				writer = csv.writer(f)
 				writer.writerow(headers)
 				f.close()
+		return False
+
+def get_latest_dispatch(out_file_path):
+	with open(out_file_path, 'r') as f:
+		reader = csv.reader(f)
+		# skip header 
+		next(reader)
+		# try to open second row and extract the last dispatch time
+		try: 
+			second_row = next(reader)
+			latest_dispatch = second_row[2]
+		except StopIteration as exception:
+			print(exception)
+			latest_dispatch = False
+
+def intialize_temp_file (temp_file_path, headers):
+	with open(temp_file_path, 'w') as f:
+		writer = csv.writer(f)
+		writer.writerow(headers)
+		f.close()
+
+def gather_latest_dispatches(temp_file_path, rows, latest_dispatch):
+	for row in rows: 
+		cells = row.find_elements(By.CSS_SELECTOR, 'td')
+		# Check if the latest data in the dispatch log matches the latest data in the sheet 
+		if cells[2].get_attribute('innerHTML') == latest_dispatch:
+			break
+		# If there is new data, write to the temp_file
+		else:
+			cell_data = []
+			for cell in cells:
+				cell_data.append(cell.get_attribute('innerHTML'))
+			# Write to file 
+			with open(temp_file_path, 'a') as f:
+				writer = csv.writer(f)
+				writer.writerow(cell_data)
+				f.close()
+
+def format_out_files(latest_dispatch, temp_file_path, out_file_path):
+	# Write old dispatch calls beneath new calls in temp file 
+	if latest_dispatch: 
+	    with open(temp_file_path, 'a') as temp_f:
+	        writer = csv.writer(temp_f)
+	        with open(out_file_path, 'r') as f:
+	            reader = csv.reader(f)
+	            next(reader)
+	            for row in reader:
+	                writer.writerow(row)
+	            f.close()
+	        temp_f.close()
+	# Write temp file into file  
+	with open(out_file_path, 'w') as f:
+	    writer = csv.writer(f)
+	    with open(temp_file_path, 'r') as temp_f:
+	        reader = csv.reader(temp_f)
+	        for row in reader:
+	            writer.writerow(row)
+	        temp_f.close()
+	    f.close()
+
+def create_metadata_file(kaggle_path, orgin_path, new_path, str_date):
+	meta_file_path = os.path.join(new_path, 'dataset-metadata.json')
+	if not os.path.exists(meta_file_path):
+		create_metadata_cmd = kaggle_path + ' datasets init -p ' + new_path
+		print(create_metadata_cmd)
+		os.system(create_metadata_cmd)
+
+		# read meta data file into dict 
+		meta_dict = {}
+		with open(orgin_path) as json_file:
+			meta_dict = json.load(json_file)
+			json_file.close()
+
+		# change meta data using dates 
+		meta_dict['title'] = meta_dict['title'] + str_date
+		meta_dict['id'] = meta_dict['id'] + str_date
+		meta_dict['resources'][0]['path'] = str_date + '.csv'
+
+		# write new meta data back into json file 
+		with open(meta_file_path, 'w') as json_file:
+			json.dump(meta_dict, json_file)
+
+
+def write_to_kaggle(is_new_dataset, kaggle_path, dataset_path):
+	# if the dataset is new, create a new dataset using cmd line 
+	if is_new_dataset:
+		# create new dataset on kaggle
+		create_dataset_cmd = kaggle_path + ' datasets create -u -p ' + dataset_path
+		os.system(create_dataset_cmd)
+	else: 
+		# tag update message with current time 
+		update_time = str(datetime.now())
+		update_msg = ' -m "Loaded new reports at' + update_time +'"'
+		# update data set 
+		update_dataset_cmd = kaggle_path + ' datasets version -p ' + dataset_path + update_msg
+		os.system(update_dataset_cmd)
+
 
